b2c93b2d73fd0f778d5a4136b5c0ebdac3b5e0fb
cesar <cesargustavo53@gmail.com>
2025-01-21 22:58:11 -0300

    build

--- src/app/shared/text-highlight.directive.ts
+++
@@ -1,7 +0,0 @@
-import { Directive } from '@angular/core';
-
-@Directive({
-	selector: '[appTextHighlight]',
-	host: { class: 'text-amber-400 font-bold' },
-})
-export class TextHighlightDirective {}
