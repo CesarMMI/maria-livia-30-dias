894c60d1583bf7a9286fce53c658cbfc6034eccd
cesar <cesargustavo53@gmail.com>
2025-02-04 01:42:24 -0300

    to scss

--- src/app/features/hero/hero.component.ts
+++ src/app/features/hero/hero.component.ts
@@ -8,23 +8,21 @@ import { TextHighlightDirective } from '../../shared/text-highlight.directive';
 	selector: 'app-hero',
 	imports: [FontAwesomeModule, ActionButtonDirective, TextHighlightDirective],
 	template: `
-		<div class="h-full max-w-5xl mx-auto">
+		<div class="cont">
 			<!-- Content -->
-			<div
-				[style.backgroundImage]="'url(images/hero.jpg)'"
-				class="h-full flex flex-col justify-end p-8 bg-center bg-cover bg-no-repeat"
-			>
-				<span class="text-center text-7xl font-black mb-4">DESAFIO<br />30 DIAS</span>
-				<span class="text-center text-2xl mb-4"
+			<div class="hero">
+				<span class="title">DESAFIO<br />30 DIAS</span>
+				<span class="subtitle"
 					>Transforme Seu Corpo<br />e Sua Vida em<br />Apenas <span appTextHighlight>4 SEMANAS!</span></span
 				>
-				<div #content class="flex flex-col justify-center gap-8">
-					<a appActionButton class="">GARANTA SUA VAGA!</a>
-					<fa-icon class="text-center text-4xl duration-1000 transition-all animate-jump" [icon]="arrowIcon"></fa-icon>
+				<div #content class="content">
+					<a appActionButton>GARANTA SUA VAGA!</a>
+					<fa-icon class="icon" [icon]="arrowIcon"></fa-icon>
 				</div>
 			</div>
 		</div>
 	`,
+	styleUrl: './hero.component.scss',
 })
 export class HeroComponent implements AfterViewInit {
 	@ViewChild('content') content?: ElementRef<HTMLDivElement>;
